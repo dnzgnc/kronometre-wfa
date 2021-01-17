388f618dbd7ebeeca6367451ef0007cc4ef4c3ba
dnzgnc <denizgenc4@hotmail.com>
2021-01-17 23:38:33 +0200

    first commit

---
+++ Kronometre-wform/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre_wform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            timer1.Interval = 1000;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayac = 0;
+            label1.Text = "0";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+        }
+    }
+}
